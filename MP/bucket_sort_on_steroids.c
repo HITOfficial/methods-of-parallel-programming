8a667c5ab69dbee31df962a4bf11bb1b255c3543
hitofficial <tomaszbochnak00@gmail.com>
2024-04-16 13:25:39 +0200

    feat: parallel bucket sort on customized buckets number

--- MP/bucket_sort_on_steroids.c
+++ MP/bucket_sort_on_steroids.c
@@ -4,15 +4,16 @@
 #include <time.h>
 
 #define BUCKET_INDEX(number, min_value, max_value, number_of_buckets) \
-    ((number - min_value) * (number_of_buckets - 1) / (max_value - min_value))
+    (((number - min_value)  / (max_value - min_value)) * (number_of_buckets - 1))
 
 int cmpfunc(const void *a, const void *b) {
     return (*(int *) a - *(int *) b);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <number of threads> <n> \n", argv[0]);
+    if (argc != 4) {
+        printf("argc: %d\n", argc);
+        printf("Usage: %s <number of threads> <number of buckets> <n> \n", argv[0]);
         return 1;
     }
     double initial_time = omp_get_wtime();
@@ -20,7 +21,8 @@ int main(int argc, char *argv[]) {
     int max = 10000000;
 
     int nthreads = atoi(argv[1]);
-    int n = atoi(argv[2]);
+    int nbuckets = atoi(argv[2]);
+    int n = atoi(argv[3]);
 
     omp_set_num_threads(nthreads);
 
@@ -50,9 +52,9 @@ int main(int argc, char *argv[]) {
     }
 
     // Allocate memory for shared buckets and their lengths
-    int *shared_bucket_length = calloc(nthreads, sizeof(int));
-    int **shared_buckets = calloc(nthreads, sizeof(int *));
-    for (int i = 0; i < nthreads; i++) {
+    int *shared_bucket_length = calloc(nbuckets, sizeof(int));
+    int **shared_buckets = calloc(nbuckets, sizeof(int *));
+    for (int i = 0; i < nbuckets; i++) {
         shared_buckets[i] = calloc(n, sizeof(int));
     }
 
@@ -61,9 +63,9 @@ int main(int argc, char *argv[]) {
 #pragma omp parallel
     {
         int thread_id = omp_get_thread_num();
-        int *bucket_length = calloc(nthreads, sizeof(int));
-        int **buckets = calloc(nthreads, sizeof(int *));
-        for (int i = 0; i < nthreads; i++) {
+        int *bucket_length = calloc(nbuckets, sizeof(int));
+        int **buckets = calloc(nbuckets, sizeof(int *));
+        for (int i = 0; i < nbuckets; i++) {
             buckets[i] = calloc(n, sizeof(int));
         }
 
@@ -71,20 +73,20 @@ int main(int argc, char *argv[]) {
         int end_index = (thread_id == nthreads - 1) ? n : n / nthreads * (thread_id + 1);
 
         for (int index = start_index; index < end_index; index++) {
-            int bucket_index = BUCKET_INDEX(tab[index], min_value_in_tab, max_value_in_tab, nthreads);
+            int bucket_index = BUCKET_INDEX(tab[index], min_value_in_tab, max_value_in_tab, nbuckets);
             buckets[bucket_index][bucket_length[bucket_index]] = tab[index];
             bucket_length[bucket_index]++;
         }
 
         // Sort each private bucket
-        for (int i = 0; i < nthreads; i++) {
+        for (int i = 0; i < nbuckets; i++) {
             qsort(buckets[i], bucket_length[i], sizeof(int), cmpfunc);
         }
 
         // Merge buckets into shared_buckets
-        for (int i = 0; i < nthreads; i++) {
+        for (int i = 0; i < nbuckets; i++) {
             for (int j = 0; j < bucket_length[i]; j++) {
-                int bucket_index = BUCKET_INDEX(buckets[i][j], min_value_in_tab, max_value_in_tab, nthreads);
+                int bucket_index = BUCKET_INDEX(buckets[i][j], min_value_in_tab, max_value_in_tab, nbuckets);
 #pragma omp critical
                 {
                     shared_bucket_length[bucket_index]++;
@@ -94,50 +96,49 @@ int main(int argc, char *argv[]) {
         }
 
         // Cleanup
-        for (int i = 0; i < nthreads; i++) {
+        for (int i = 0; i < nbuckets; i++) {
             free(buckets[i]);
         }
         free(buckets);
         free(bucket_length);
     }
+
     end = omp_get_wtime();
     double bucket_distribution_private_sort = end - start;
 
 
     start = omp_get_wtime();
 // Sort shared buckets
-#pragma omp parallel
-    {
-        int i = omp_get_thread_num();
-        qsort(shared_buckets[i], shared_bucket_length[i], sizeof(int), cmpfunc);
+#pragma omp parallel for
+        for (int i = 0; i < nbuckets; i++) {
+            qsort(shared_buckets[i], shared_bucket_length[i], sizeof(int), cmpfunc);
     }
-
     end = omp_get_wtime();
+
     double shared_bucket_sort = end - start;
 
 
     start = omp_get_wtime();
-//  Merge sorted buckets back into the original array
-#pragma omp parallel
-    {
-        int i = omp_get_thread_num();
+#pragma omp parallel for
+    for (int b = 0; b < nbuckets; b++) {
         int previous_buckets_length = 0;
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < b; j++) {
             previous_buckets_length += shared_bucket_length[j];
         }
-        for (int j = 0; j < shared_bucket_length[i]; j++) {
+        for (int j = 0; j < shared_bucket_length[b]; j++) {
             int index = j + previous_buckets_length;
-            tab[index] = shared_buckets[i][j];
+            tab[index] = shared_buckets[b][j];
         }
     }
+
     end = omp_get_wtime();
     double initial_table_merge_bucket = end - start;
 
     double total_runtime = end - initial_time;
-    printf("%d %d %f %f %f %f %f\n", nthreads, n, generating_random_numbers, bucket_distribution_private_sort,
+    printf("%d %d %d %f %f %f %f %f\n", nthreads, nbuckets, n, generating_random_numbers, bucket_distribution_private_sort,
            shared_bucket_sort, initial_table_merge_bucket, total_runtime);
 
-    for (int i = 0; i < nthreads; i++) {
+    for (int i = 0; i < nbuckets; i++) {
         free(shared_buckets[i]);
     }
     free(shared_buckets);
--- MP/exp.run.sh
+++ MP/exp.run.sh
@@ -2,15 +2,17 @@
 rm -f output.csv
 touch output.csv
 
-echo "threads,n,generating_random_numbers,bucket_distribution_private_sort,shared_bucket_sort,initial_table_merge_bucket,total_runtime," > "output.csv"
+echo "threads,n,buckets,generating_random_numbers,bucket_distribution_private_sort,shared_bucket_sort,initial_table_merge_bucket,total_runtime," > "csv/output.csv"
 
-gcc -Wall rand.c -o rand -fopenmp -DPADDING=0
+gcc -Wall bucket_sort_on_steroids.c -o bucket_sort_on_steroids -fopenmp -DPADDING=0
 
 for threads in {1..8}; do
-  for n in {1000000..10000000..1000000}; do
-    for repetition in {1..10}; do
-      echo "Run number $repetition for $threads threads and $n numbers"
-      ./rand $threads $n >> "output.csv";
+  for buckets in {100..1000..100}; do
+    for n in {1000000..10000000..1000000}; do
+      for repetition in {1..3}; do
+        echo "Run number $repetition for $threads threads $buckets buckets and $n numbers"
+        ./bucket_sort_on_steroids $threads $buckets $n >> "output.csv";
+      done
     done
   done
 done
